b8023f8d9d2f96857ab8bacb71c2b7d3cde8e505
benmacomb <benmacomb@u.boisestate.edu>
2018-10-05 09:44:16 -0600

    Scripts

---
+++ Test_Project/Assets/Scripts/LineAgent.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineAgent : MonoBehaviour {
+
+	public Transform target;
+    private LineRenderer lineRenderer;
+    private UnityEngine.AI.NavMeshPath path;
+    private float elapsed = 0.0f;
+	// Use this for initialization
+	void Start () {
+        lineRenderer = gameObject.AddComponent<LineRenderer>() as LineRenderer;
+        lineRenderer.startWidth = .5f;
+        lineRenderer.endWidth = .5f;
+		path = new UnityEngine.AI.NavMeshPath();
+        UnityEngine.AI.NavMesh.CalculatePath(transform.position, target.position, UnityEngine.AI.NavMesh.AllAreas, path);
+        lineRenderer.SetVertexCount(path.corners.Length);
+        lineRenderer.SetPositions(path.corners);
+        elapsed = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Update the way to the goal every second.
+        elapsed += Time.deltaTime;
+        if (elapsed > 1.0f)
+        {
+            elapsed -= 1.0f;
+        }
+            UnityEngine.AI.NavMesh.CalculatePath(transform.position, target.position, UnityEngine.AI.NavMesh.AllAreas, path);
+             lineRenderer.SetVertexCount(path.corners.Length);
+
+        lineRenderer.SetPositions(path.corners);
+
+        for (int i = 0; i < path.corners.Length - 1; i++)
+            Debug.DrawLine(path.corners[i], path.corners[i + 1], Color.red);
+
+    }
+}
---
+++ Test_Project/Assets/Scripts/VRTouchpadRotate.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/* Class that rotates the scene around the user with the touchpad about the y-axis*/
+public class VRTouchpadRotate : MonoBehaviour
+{
+    [SerializeField]
+    private Transform rig;
+    private Valve.VR.EVRButtonId touchpad = Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad;
+    private SteamVR_TrackedObject trackedObj;
+    public float speed = 1f;
+
+    private SteamVR_Controller.Device controller
+    {
+        get
+        {
+            return SteamVR_Controller.Input((int)trackedObj.index);
+        }
+    }
+
+    private Vector2 axis = Vector2.zero;
+
+    void Start()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+
+    void Update()
+    {
+        if (controller == null)
+        {
+            Debug.Log("Controller not initialized.");
+            return;
+        }
+
+        var device = SteamVR_Controller.Input((int)trackedObj.index);
+
+        
+        //Rotate rig around camera eye position when touchpad is active.
+        if (controller.GetTouch(touchpad))
+        {
+            axis = device.GetAxis(Valve.VR.EVRButtonId.k_EButton_Axis0);
+
+            if (rig != null)
+            {     
+                var cameraObj = GameObject.Find("Camera (eye)");
+                rig.RotateAround(cameraObj.transform.position,
+                                 new Vector3(0,axis.x, 0), 20f * Time.deltaTime * speed);          
+            }
+        }
+    }
+
+}
