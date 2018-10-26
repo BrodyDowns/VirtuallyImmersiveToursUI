f5697c551a9a3c7d320f0ebfa9ed5b5464d43d5e
Brody Downs <brodydowns@u.boisestate.edu>
2018-10-26 10:26:04 -0600

    Teleport Button

---
+++ Test_Project/Assets/Scripts/TeleportButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TeleportButton : MonoBehaviour {
+
+    public string sceneName = "";
+    public GameObject player;
+
+    public void Teleport()
+    {
+        StartCoroutine(LoadYourAsyncScene());
+    }
+
+    IEnumerator LoadYourAsyncScene()
+    {
+
+        DontDestroyOnLoad(player);
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
+
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+
+        SceneManager.MoveGameObjectToScene(player, SceneManager.GetSceneByName(sceneName));
+        SceneManager.SetActiveScene(SceneManager.GetSceneByName(sceneName));
+    }
+}
